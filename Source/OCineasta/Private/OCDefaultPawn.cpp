15d996af91478fa275f15e5b1467f96680d640d6
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-08-24 02:29:59 -0300

    [ADDED] Classe genérica de peão para o jogador

---
+++ Source/OCineasta/Private/OCDefaultPawn.cpp
@@ -0,0 +1,13 @@
+
+
+#include "OCineasta.h"
+#include "OCDefaultPawn.h"
+
+
+AOCDefaultPawn::AOCDefaultPawn(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/OCineasta/Public/OCDefaultPawn.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/DefaultPawn.h"
+#include "OCDefaultPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OCINEASTA_API AOCDefaultPawn : public ADefaultPawn
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
