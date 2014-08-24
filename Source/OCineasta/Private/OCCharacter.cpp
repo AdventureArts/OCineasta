6e5ca91521f2616392dc645b26994a134c65dbae
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-08-24 00:20:55 -0300

    [ADDED] Classe genérica de personagem

---
+++ Source/OCineasta/Private/OCCharacter.cpp
@@ -0,0 +1,13 @@
+
+
+#include "OCineasta.h"
+#include "OCCharacter.h"
+
+
+AOCCharacter::AOCCharacter(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/OCineasta/Public/OCCharacter.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/Character.h"
+#include "OCCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OCINEASTA_API AOCCharacter : public ACharacter
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
