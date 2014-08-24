2997bca1d86b635cd1d3aed5ec64fdae732133ff
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-08-24 00:24:42 -0300

    [ADDED] Classe genérica de HUD

---
+++ Source/OCineasta/Private/OCHUD.cpp
@@ -0,0 +1,13 @@
+
+
+#include "OCineasta.h"
+#include "OCHUD.h"
+
+
+AOCHUD::AOCHUD(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/OCineasta/Public/OCHUD.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "OCHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OCINEASTA_API AOCHUD : public AHUD
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
