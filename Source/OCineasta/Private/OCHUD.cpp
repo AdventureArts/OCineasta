c97df038e75d2da79c495ecb0c584a20c5885ad2
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-08-25 02:50:03 -0300

    Cursor agora é impresso na tela

--- Source/OCineasta/Private/OCHUD.cpp
+++ Source/OCineasta/Private/OCHUD.cpp
@@ -7,7 +7,33 @@
 AOCHUD::AOCHUD(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
+	// ====================
+	// Configuração de cursor temporário para o protótipo da versão a0.1.
+	// ====================
 
+	cursorTexture = ConstructorHelpers::FObjectFinder<UTexture>(TEXT("/Engine/EditorMaterials/TargetIcon.TargetIcon")).Object;
+
+	float cursorWidth = cursorTexture->GetSurfaceWidth();
+	float cursorHeight = cursorTexture->GetSurfaceHeight();
+
+	cursorSize = FVector2D(cursorWidth, cursorHeight) / 4.f;
+	cursorCentre = cursorSize / 2.f;
+	cursorLocation = FVector2D(0.f, 0.f);
+
+	// ====================
+	// Fim de configuração de cursor.
+	// ====================
 }
 
+void AOCHUD::DrawHUD()
+{
+	Super::DrawHUD();
 
+	// Atualiza a coordenada que representa o centro da tela.
+	screenCentre = FVector2D(Canvas->ClipX, Canvas->ClipY) / 2.f;
+
+	if (cursorTexture) // Existe a textura de cursor para ser impressa na tela?
+	{
+		Canvas->K2_DrawTexture(cursorTexture, screenCentre - cursorCentre, cursorSize, FVector2D(0.f, 0.f));
+	}
+}
--- Source/OCineasta/Public/OCHUD.h
+++ Source/OCineasta/Public/OCHUD.h
@@ -13,6 +13,15 @@ class OCINEASTA_API AOCHUD : public AHUD
 {
 	GENERATED_UCLASS_BODY()
 
+	UTexture *cursorTexture;
+	FVector2D cursorSize;
+	FVector2D cursorLocation;
+	FVector2D cursorCentre;
 	
+	FVector2D screenCentre;
 	
+
+protected:
+
+	virtual void DrawHUD() override;
 };
