265e5901b63072ab8b2ca9b347ee68abb1799189
Vinícius Rodrigues dos Santos <vinicius-rs@hotmail.com.br>
2014-09-05 03:11:17 -0300

    Controlador de jogador agora gerencia entradas

--- Source/OCineasta/Private/OCPlayerController.cpp
+++ Source/OCineasta/Private/OCPlayerController.cpp
@@ -7,7 +7,50 @@
 AOCPlayerController::AOCPlayerController(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
-
+	InputComponent = ConstructObject<UInputComponent>(UInputComponent::StaticClass(), this, TEXT("ControllerInputComponent"));
 }
 
 
+void AOCPlayerController::BeginPlay()
+{
+	SetupControllerInputComponent(InputComponent);
+}
+
+void AOCPlayerController::SetupControllerInputComponent(UInputComponent* InputComponent)
+{
+	check(InputComponent);
+
+	InputComponent->BindAxis("mouseX", this, &AOCPlayerController::mouseX);
+	InputComponent->BindAxis("mouseY", this, &AOCPlayerController::mouseY);
+	InputComponent->BindAxis("forward", this, &AOCPlayerController::forward);
+	InputComponent->BindAxis("right", this, &AOCPlayerController::right);
+}
+
+void AOCPlayerController::mouseX(float input)
+{
+
+}
+
+void AOCPlayerController::mouseY(float input)
+{
+
+}
+
+void AOCPlayerController::forward(float input)
+{
+	FVector viewDirection = GetControlRotation().Vector().SafeNormal();
+
+	APawn *controlledPawn = GetPawn();
+
+	if (controlledPawn) controlledPawn->AddMovementInput(viewDirection, input * 0.5f);
+}
+
+void AOCPlayerController::right(float input)
+{
+	FRotator viewRotator = GetControlRotation().Add(0.f, 90.f, 0.f);
+	FVector viewDirection = viewRotator.Vector().SafeNormal();
+
+	APawn *controlledPawn = GetPawn();
+
+	if (controlledPawn) controlledPawn->AddMovementInput(viewDirection, input * 0.5f);
+}
--- Source/OCineasta/Public/OCPlayerController.h
+++ Source/OCineasta/Public/OCPlayerController.h
@@ -14,5 +14,15 @@ class OCINEASTA_API AOCPlayerController : public APlayerController
 	GENERATED_UCLASS_BODY()
 
 	
+protected:
+
+	virtual void BeginPlay();
+
+	virtual void SetupControllerInputComponent(UInputComponent* InputComponent);
 	
+	virtual void mouseX(float input);
+	virtual void mouseY(float input);
+	virtual void forward(float input);
+	virtual void right(float input);
+
 };
